f9c600d7cd8336b92f261be38f299f12e748286d
Aleksey Afonin <aloxeika@gmail.com>
2024-10-10 17:25:21 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://68177bc6-e6cb-45ca-a368-d20272fb0f32.serverhub.praktikum-services.ru"
+
+CREATE_USER_PATH = "/api/v1/users/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
